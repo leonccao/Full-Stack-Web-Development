cf33f1b23961176868ed39caaccc02873d6f56b5
Minty <mintyck@gmail.com>
2017-10-14 22:33:55 +0800

    Single page application part 2

--- Angular/conFusion/src/app/app-routing/routes.ts
+++ Angular/conFusion/src/app/app-routing/routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'menu', component: MenuComponent },
+  { path: 'dishdetail/:id', component: DishdetailComponent },
   { path: 'contactus', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
