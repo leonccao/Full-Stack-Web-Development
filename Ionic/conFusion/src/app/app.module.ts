0ef7ec76bf20a28a591e5e43e0f97807cb1e3111
Minty <mintyck@gmail.com>
2017-10-28 22:24:34 +0800

    Add social sharing

--- Ionic/conFusion/src/app/app.module.ts
+++ Ionic/conFusion/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LocalNotifications } from '@ionic-native/local-notifications';
 import { EmailComposer } from '@ionic-native/email-composer';
+import { SocialSharing } from '@ionic-native/social-sharing';
 import { DishProvider } from '../providers/dish/dish';
 import { LeaderProvider } from '../providers/leader/leader';
 import { PromotionProvider } from '../providers/promotion/promotion';
@@ -64,6 +65,7 @@ import { FavoriteProvider } from '../providers/favorite/favorite';
     SplashScreen,
     LocalNotifications,
     EmailComposer,
+    SocialSharing,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     DishProvider,
     LeaderProvider,
--- Ionic/conFusion/src/pages/dishdetail/dishdetail.ts
+++ Ionic/conFusion/src/pages/dishdetail/dishdetail.ts
@@ -5,6 +5,7 @@ import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
 import { Platform, ActionSheetController } from 'ionic-angular';
 import { CommentPage } from '../../pages/comment/comment';
+import { SocialSharing } from '@ionic-native/social-sharing';
 
 /**
  * Generated class for the DishdetailPage page.
@@ -30,7 +31,8 @@ export class DishdetailPage {
     private toastCtrl: ToastController,
     private platform: Platform,
     private actionsheetCtrl: ActionSheetController,
-    private modalctrl: ModalController) {
+    private modalctrl: ModalController,
+    private socialSharing: SocialSharing) {
       this.dish = navParams.get('dish');
       this.favorite = favoriteservice.isFavorite(this.dish.id);
       this.numcomments = this.dish.comments.length;
@@ -67,6 +69,22 @@ export class DishdetailPage {
           text: 'Add a Comment',
           handler: () => {
             this.openModal();
+          }
+        },
+        {
+          text: 'Share via Facebook',
+          handler: () => {
+            this.socialSharing.shareViaFacebook(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Facebook'))
+              .catch(() => console.log('Failed to post to Facebook'));
+          }
+        },
+        {
+          text: 'Share via Twitter',
+          handler: () => {
+            this.socialSharing.shareViaTwitter(this.dish.name + ' -- ' + this.dish.description, this.BaseURL + this.dish.image, '')
+              .then(() => console.log('Posted successfully to Twitter'))
+              .catch(() => console.log('Failed to post to Twitter'));
           }
         },
         {
