728c168325800636222e8a39baf9931bca1e9e4f
Minty <mintyck@gmail.com>
2017-11-13 14:03:35 +0800

    HTTPS

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -26,6 +26,17 @@ connect.then((db) => {
 }, (err) => { console.log(err); });
 
 var app = express();
+
+// Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
