9380bf4a75cc1a97b8c32ccf38671fa056217490
Minty <mintyck@gmail.com>
2017-11-13 18:14:37 +0800

    Support image uploading

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -14,6 +14,8 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
+
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird'); 
 const url = config.mongoUrl;
@@ -62,6 +64,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
