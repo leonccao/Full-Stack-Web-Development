118558e14dd6768ba762b37ac128a5413a818ee3
Minty <mintyck@gmail.com>
2017-11-13 23:44:37 +0800

    Support login by Facebook OAuth

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '317615258719215',
+        clientSecret: 'e7dfb335e43951043d127dd95d4f9af7'
+    } 
 }
